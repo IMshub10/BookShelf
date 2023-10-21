d975d96fd3a7c7f6bd46ab43baae20cd03737551
kb-shub <54928767+IMshub10@users.noreply.github.com>
2023-10-21 17:06:40 +0530

    AppDao Updated

--- app/src/main/java/com/summer/bookshelf/persistence/db/daos/AppDao.kt
+++ app/src/main/java/com/summer/bookshelf/persistence/db/daos/AppDao.kt
@@ -1,8 +1,22 @@
 package com.summer.bookshelf.persistence.db.daos
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.summer.bookshelf.persistence.db.entities.CountryEntity
+import com.summer.bookshelf.persistence.db.entities.UserEntity
 
 @Dao
 interface AppDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertIgnoreCountry(countryEntity: CountryEntity)
+
+    @Query("Select * from countries order by name")
+    fun fetchCountries(): List<CountryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUser(userEntity: UserEntity)
 
 }
